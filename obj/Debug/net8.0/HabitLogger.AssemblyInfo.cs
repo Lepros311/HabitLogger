3244dbeda349ece4dcd42d5901da69173f368349
Lepros311 <andy.strickland@gmail.com>
2024-07-17 23:30:55 -0400

    Initial commit

--- obj/Debug/net8.0/HabitLogger.AssemblyInfo.cs
+++ obj/Debug/net8.0/HabitLogger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d48e7facd1332bddd162036086484b580798f3a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ee930c21b635bd4d0b436b8b66cb7380415633")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
