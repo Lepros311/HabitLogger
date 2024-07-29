610b555cef010552af9332666b3cf8b4dcc2efb4
Lepros311 <andy.strickland@gmail.com>
2024-07-29 10:50:48 -0400

    Add user menu and option functions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? userMenuChoice;
+bool closeApp = false;
+
+// Function for user to select menu option
+void UserSelectMenuOption()
+{
+    Console.WriteLine("\nMAIN MENU\n");
+    Console.WriteLine("What would you like to do?\n");
+    Console.WriteLine("Type 0 to Close Application.");
+    Console.WriteLine("Type 1 to View All Records.");
+    Console.WriteLine("Type 2 to Insert Record.");
+    Console.WriteLine("Type 3 to Delete Record.");
+    Console.WriteLine("Type 4 to Update Record.\n");
+    string[] menuOptions = { "0", "1", "2", "3", "4" };
+    do
+    {
+        userMenuChoice = Console.ReadLine();
+        if (!menuOptions.Contains(userMenuChoice))
+        {
+            Console.WriteLine("That is not a valid option. You must enter 0, 1, 2, 3, or 4.");
+        }
+    } while (!menuOptions.Contains(userMenuChoice));
+}
+
+// Function to close application (0)
+void CloseApplication()
+{
+    closeApp = true;
+    Console.WriteLine("Goodbye!");
+}
+
+// Function to view all records (1)
+void ViewAllRecords()
+{
+    Console.WriteLine("Here's all your records:");
+}
+
+// Function to insert record (2)
+void InsertRecord()
+{
+    Console.WriteLine("Let's insert a record...");
+}
+
+// Function to delete record (3)
+void DeleteRecord()
+{
+    Console.WriteLine("Let's delete a record...");
+}
+
+// Function to update record (4)
+void UpdateRecord()
+{
+    Console.WriteLine("Let's update a record...");
+}
+
+// Habit Logger 
+
+do
+{
+    UserSelectMenuOption();
+
+    switch (userMenuChoice)
+    {
+        case "0":
+            CloseApplication();
+            break;
+        case "1":
+            ViewAllRecords();
+            break;
+        case "2":
+            InsertRecord();
+            break;
+        case "3":
+            DeleteRecord();
+            break;
+        case "4":
+            UpdateRecord();
+            break;
+    }
+
+} while (closeApp == false);
--- obj/Debug/net8.0/HabitLogger.AssemblyInfo.cs
+++ obj/Debug/net8.0/HabitLogger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ee930c21b635bd4d0b436b8b66cb7380415633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3244dbeda349ece4dcd42d5901da69173f368349")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
